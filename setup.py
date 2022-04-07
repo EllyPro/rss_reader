d7773a264605f75ff6d0db210b222458a4b09216
Elyorbek Hamroyev <pro100elly@gmail.com>
2022-04-07 11:06:08 +0300

    Iter 2

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r", encoding="utf-8") as fh:
+    requirements = fh.read()
+setup(
+    name='elyorbeks-rss-reader',
+    version='0.0.1',
+    author='Elyorbek Hamroyev',
+    author_email='pro100elly@gmail.com',
+    license='MIT',
+    description='This cmd tool is intended to read rss-feed from realpython.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/EllyPro',
+    py_modules=['rss_reader', 'app'],
+    packages=find_packages(),
+    install_requires=[requirements],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    entry_points='''
+        [console_scripts]
+        rss_reader=rss_reader:main
+    '''
+)
